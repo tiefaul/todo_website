ff44514a71be54848d675d2157175a4388239868
TieFaul <tylerfaulhaber@gmail.com>
2024-11-13 13:18:28 -0600

    "Updated settings.py: changed DEBUG to True, modified ALLOWED_HOSTS, and added trailing slash to STATIC_ROOT"

--- todo_website/settings.py
+++ todo_website/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('IP')]
-
+ALLOWED_HOSTS = ['*', os.getenv('IP')]
+# ALLOWED_HOSTS = [os.getenv('IP')]
 
 # Application definition
 
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = './static'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
