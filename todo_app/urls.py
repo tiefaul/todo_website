60292b74eee6f3965497597d5b7f893d09f342f4
TieFaul <tylerfaulhaber@gmail.com>
2024-11-13 16:22:00 -0600

    base login page set up.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.ListListView.as_view(), name='index'), # name parameter is used for the templates so they have something to point to
-    path('list/<int:list_id>/', views.ItemListView.as_view(), name='list',),
+    path('todo-app/', views.ListListView.as_view(), name='index'), # name parameter is used for the templates so they have something to point to
+    path('todo-app/list/<int:list_id>/', views.ItemListView.as_view(), name='list',),
     # CRUD patterns for ToDoLists
-    path('list/add/', views.ListCreate.as_view(), name="list-add",),
-    path("list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete",),
+    path('todo-app/list/add/', views.ListCreate.as_view(), name="list-add",),
+    path("todo-app/list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete",),
     # CRUD patterns for ToDoItems
-    path('list/<int:list_id>/item/add/', views.ItemCreate.as_view(), name='item-add',),
-    path('list/<int:list_id>/item/<int:pk>/', views.ItemUpdate.as_view(), name='item-update',),
-    path("list/<int:list_id>/item/<int:pk>/delete/", views.ItemDelete.as_view(), name="item-delete",),
+    path('todo-app/list/<int:list_id>/item/add/', views.ItemCreate.as_view(), name='item-add',),
+    path('todo-app/list/<int:list_id>/item/<int:pk>/', views.ItemUpdate.as_view(), name='item-update',),
+    path("todo-app/list/<int:list_id>/item/<int:pk>/delete/", views.ItemDelete.as_view(), name="item-delete",),
 ]
